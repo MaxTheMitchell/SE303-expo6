d3bd86bc1f618709e30858566cb2ffa2b3fa0c7b
MaxTheMitchell <rat2043@gmail.com>
2021-05-05 15:44:59 -0700

    remove refrences to 'button' from method signatures

--- main.rb
+++ main.rb
@@ -8,17 +8,17 @@ class Button
      @foreground_color = foreground_color
   end
 
-  def draw_button_in_light_mode
-    draw_shaded_button(foreground_color + 10, '#E0E0E0')
+  def draw_in_light_mode
+    draw_shaded(foreground_color + 10, '#E0E0E0')
   end
 
-  def draw_button_in_dark_mode
-    draw_shaded_button(foreground_color - 10,  '#111111')
+  def draw_in_dark_mode
+    draw_shaded(foreground_color - 10,  '#111111')
   end
 
   private 
 
-  def draw_shaded_button(foreground_color, background_color)
+  def draw_shaded(foreground_color, background_color)
      paint(label_text, x, y, foreground_color, background_color)
   end
 end
