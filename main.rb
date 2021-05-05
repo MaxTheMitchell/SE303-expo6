cb56a7bbdaf0376880a9d7a05c81b16000373a23
MaxTheMitchell <rat2043@gmail.com>
2021-05-05 15:29:30 -0700

    apply introduce parameter object

--- main.rb
+++ main.rb
@@ -1,7 +1,18 @@
-def draw_button_in_light_mode(label_text, x, y, foreground_color)
-  paint(label_text, x, y, foreground_color + 10, '#E0E0E0')
+def draw_button_in_light_mode(button)
+  paint(button.label_text, button.x, button.y, button.foreground_color + 10, '#E0E0E0')
 end
 
-def draw_button_in_dark_mode(label_text, x, y, foreground_color)
-  paint(label_text, x, y, foreground_color - 10,  '#111111')
+def draw_button_in_dark_mode(button)
+  paint(button.label_text, button.x, button.y, button.foreground_color - 10,  '#111111')
+end
+
+class Button 
+  attr_reader :label_text, :x, :y, :foreground_color
+
+  def intialize(label_text, x, y, foreground_color)
+     @label_text = label_text
+     @x = x
+     @y = y
+     @foreground_color = foreground_color
+  end
 end
